f301c3da1c3c471a8ecca9a6e33e596bf683a37e
Felipe <felipe.augusto.rodrigues1@gmail.com>
2026-06-05 16:10:40 -0300

    feat: port design-handoff theme to Tailwind v4 tokens, bundle fonts locally

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,5 +1,17 @@
 import "./global.css"
 
+import "@fontsource/poppins/300.css"
+import "@fontsource/poppins/400.css"
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/600.css"
+import "@fontsource/poppins/700.css"
+import "@fontsource/jetbrains-mono/400.css"
+import "@fontsource/jetbrains-mono/500.css"
+import "@fontsource/jetbrains-mono/600.css"
+import "@fontsource/jetbrains-mono/700.css"
+import "@fontsource/instrument-serif/400.css"
+import "@fontsource/instrument-serif/400-italic.css"
+
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
